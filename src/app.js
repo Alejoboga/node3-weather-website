2434218072d5279d4aaed82f9ea413db6f0be8ca
Boga <alejo.boga@sovos.com>
2019-08-23 15:15:49 -0300

    fixing program to work with heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000   //seteamos el puerto por el valor provisto por heroku y seteamos por defecto el 3000 por si heroku no nos da el valor
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -97,6 +98,6 @@ app.get('*', (req,res) =>{
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
